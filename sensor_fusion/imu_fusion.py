009159bf786c6ba7fb3f8fe7016f650327055bbc
Pierre-Minh <pierre.minh9009@gmail.com>
2025-12-25 18:55:30 +0100

    dsadsa

--- sensor_fusion/imu_fusion.py
+++ sensor_fusion/imu_fusion.py
@@ -108,8 +108,8 @@ def run_imu_fusion():
     # -------------------------
     ekf = EKF(
         gyr=np.zeros((1, 3)),
-        acc=acc0,
-        mag=mag0,  # En nT maintenant
+        acc=acc0.reshape((1,3)),
+        mag=mag0.reshape((1,3)),  # En nT maintenant
         frequency=100.0,
         frame="NED",
         magnetic_ref=mag_ref_ned,
